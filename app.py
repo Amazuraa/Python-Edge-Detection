c501ef5cc6bb2ea0c495337b6e3accd762185591
unknown <amazuraa@gmail.com>
2022-04-12 17:07:10 +0700

    Update Main Logic

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, send_file, send_from_directory
+import cv2 as cv;
+import io;
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path = "/uploads", static_folder = "uploads")
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -10,15 +11,35 @@ def homepage():
 @app.route('/upload', methods=['POST'])
 def upload():
      # -- uploaded file setup
-     file = request.files['fileInput']
+     file = request.files.get('fileInput')
      file_path = "./uploads/img/" + file.filename
      file.save(file_path)
 
-     # -- compression setup
-     # get_name = file.filename.split(".")[0]
-     # compression(file_path, get_name)
+     # Canny edge detection --------------------------------------
+     img = cv.imread(file_path, 0)
+     edges = cv.Canny(img,100,200)
 
-     return render_template('index.html')
+     cv.imwrite("./uploads/canny/" + file.filename, edges)
+
+     # Sobel edge detection --------------------------------------
+     img2 = cv.imread(file_path, 1)
+     src = cv.GaussianBlur(img2, (3, 3), 0)
+     grays = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+     sobelx = cv.Sobel(grays, cv.CV_64F, dx=1, dy=0, ksize=3) # on X axis
+     sobely = cv.Sobel(grays, cv.CV_64F, dx=0, dy=1, ksize=3) # on Y axis
+
+     abs_grad_x = cv.convertScaleAbs(sobelx)
+     abs_grad_y = cv.convertScaleAbs(sobely)
+
+     grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+     cv.imwrite("./uploads/sobel/" + file.filename, grad)
+
+     return {
+          "sobel" : "./uploads/sobel/" + file.filename,
+          "canny" : "./uploads/canny/" + file.filename
+     }
 
 @app.route('/test', methods=['GET'])
 def sobel():
